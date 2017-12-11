a7df3656c1fe227c20ce4c179231d09e310816b8
Nick Ye <nick.ye888@gmail.com>
2017-12-11 21:47:25 +1300

    Added first top down test

---
+++ MineSweeperKata/MineSweeperKata.Spec/Unit Tests/MineSweeperShould.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace MineSweeperKata.Spec.Unit_Tests
+{
+    [TestFixture]
+    public class MineSweeperShould
+    {
+        [Test]
+        public void RevealMineAndDetectNearbyMines()
+        {
+            var field = "22\n" +
+                        "..\n" +
+                        ".*\n" +
+                        "00\n";
+
+            var expected = "Field #1:\n" +
+                         "11\n" +
+                         "1*\n";
+
+            var metalDetector = new MetalDetector();
+
+            var actual = metalDetector.Detect(field);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ MineSweeperKata/MineSweeperKata/MetalDetector.cs
@@ -0,0 +1,16 @@
+﻿namespace MineSweeperKata
+{
+    public class MetalDetector
+    {
+        public MetalDetector()
+        {
+        }
+
+        public string Detect(string field)
+        {
+            return "Field #1:\n" +
+                   "11\n" +
+                   "1*\n";
+        }
+    }
+}
