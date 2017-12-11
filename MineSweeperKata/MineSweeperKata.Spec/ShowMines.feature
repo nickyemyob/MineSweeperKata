d6ab29fe613fd4686bf61a34c12d93ccd955a11f
Nick Ye <nick.ye888@gmail.com>
2017-12-11 22:27:41 +1300

    Added acceptance test scenario

---
+++ MineSweeperKata/MineSweeperKata.Spec/ShowMines.feature
@@ -0,0 +1,9 @@
+﻿Feature: ShowMines
+	As a minesweeper
+	I want to see where mines are on the field
+
+@mytag
+Scenario: See where mines are
+	Given I have a 2 x 2 field with 1 mines
+	When I run the metal detector over the field
+	Then the result should be a field with 1 revealed
---
+++ MineSweeperKata/MineSweeperKata.Spec/ShowMinesSteps.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace MineSweeperKata.Spec
+{
+    [Binding]
+    public class ShowMinesSteps
+    {
+        [Given(@"I have a (.*) x (.*) field with (.*) mines")]
+        public void GivenIHaveAXFieldWithMines(int p0, int p1, int p2)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I run the metal detector over the field")]
+        public void WhenIRunTheMetalDetectorOverTheField()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be a field with (.*) revealed")]
+        public void ThenTheResultShouldBeAFieldWithRevealed(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
