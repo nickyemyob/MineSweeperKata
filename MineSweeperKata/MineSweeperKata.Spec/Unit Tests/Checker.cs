58275c457d0955bde31896640fc7d8e7f0ad46c4
Nick Ye <nick.ye888@gmail.com>
2017-12-16 21:47:27 +1300

    Created checker class and test for checking if mine is at next the right edge of the board

---
+++ MineSweeperKata/MineSweeperKata.Spec/Unit Tests/Checker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MineSweeperKata.Spec.Unit_Tests
+{
+    internal class Checker
+    {
+        public Checker()
+        {
+        }
+
+        public bool IsAtEdge(IEnumerable<IEnumerable<char>> field)
+        {
+            return true;
+        }
+    }
+}
---
+++ MineSweeperKata/MineSweeperKata.Spec/Unit Tests/CheckerShould.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MineSweeperKata.Spec.Unit_Tests
+{
+    [TestFixture]
+    public class CheckerShould
+    {
+        private static readonly object[] FieldObjects =
+        {
+            new object[]
+            {
+                new List<string>
+                {
+                    "..",
+                    ".*"
+                }
+            }
+        };
+
+        [TestCaseSource(nameof(FieldObjects))]
+        [Test]
+        public void ReturnTrueIfMineIsNextToTheRightEdgeOfTheBoard(IEnumerable<IEnumerable<char>> inputField)
+        {
+            var field = inputField;
+            var checker = new Checker();
+
+            var isAtEdge = checker.IsAtEdge(field);
+
+            Assert.True(isAtEdge);
+
+
+        }
+
+    }
+}
