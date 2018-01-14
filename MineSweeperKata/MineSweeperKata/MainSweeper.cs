c8236522fd16667633380648e03a8b84e2cd1833
nickye888 <nickye888@gmail.com>
2018-01-14 23:34:57 +1300

    loop through input and assign fields

--- MineSweeperKata/MineSweeperKata/MainSweeper.cs
+++ MineSweeperKata/MineSweeperKata/MainSweeper.cs
@@ -10,22 +10,40 @@ namespace MineSweeperKata
 
         public string Sweep(string inputField)
         {
-            var inputReader = inputField.Split('\n');
+            var RawInputData = inputField.Split('\n');
+            var field = new Field();
+            int fieldHeight = 0;
+            int fieldWidth = 0;
+            var fieldLayout = new List<string>();
 
-            foreach (var fieldInfo in inputReader)
+            foreach (var fieldInfo in RawInputData)
             {
+                
                 if (fieldInfo.Any(char.IsDigit))
                 {
                     if (fieldInfo == EndOfInput)
                     {
                         break;
                     }
+                    
+                    //TODO: CHECK FOR "01"
+
+                    //assigning fields here
+                    if (fieldHeight > 0 && fieldWidth > 0)
+                    {
+                        field.Height = fieldHeight;
+                        field.Width = fieldWidth;
+                        field.FieldLayout = fieldLayout;
+                    }
                 }
 
-                var field = new Field();
-                var fieldLayout = new List<string> {fieldInfo};
-                field.FieldLayout = fieldLayout;
+                fieldHeight++;
+                fieldWidth = fieldInfo.Length;
+                fieldLayout.Add(fieldInfo);
             }
+
+            
+
             return "Field #1:\n" +
                    "11\n" +
                    "1*\n";
